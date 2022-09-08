4c4adc182c18fbefe5d7ec0f174eea331a3e0f3e
semmygram <semiukayode254@gmail.com>
2022-09-08 12:43:34 -0700

    gh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 /**
  * main - prints a string 
  *
- * return : always (0)
+ * return :(0)
  */
 
 int main(void) 
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	getchar();
 	return 0;
