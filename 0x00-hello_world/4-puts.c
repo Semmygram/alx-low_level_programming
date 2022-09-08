01534b7efbcf25f31b7554faa18d8933d6dd87c4
semmygram <semiukayode254@gmail.com>
2022-09-08 13:06:38 -0700

    push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - entrypoint
  *
- * Return: "always 0"
+ * Return: always 0
  */
 
 int main(void)
@@ -10,5 +10,5 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 
 	getchar();
-	return 0;
+	return(0);
 }
