2b4389a840fd981849fdb1241a923abc9c95eacf
semmygram <semiukayode254@gmail.com>
2022-09-09 16:47:03 -0700

    it works

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
