b2ae5149e0018e30a520abd08a461bb9535500b2
semmygram <semiukayode254@gmail.com>
2022-09-09 16:02:30 -0700

    it works

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all the alphabet
+ *
+ *Return: 0
+ *
+ */
+
+int main(void)
+{
+ 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	 int i ;
+	 for (i = 0; i < 26; i++)
+	 {
+		 putchar(alp[i]);
+	 }
+	 putchar("\n");
+	 return (0);
+}
