5568c4651a4e1ddd238d826ebb428a0c0541dbc5
semmygram <semiukayode254@gmail.com>
2022-09-10 06:23:29 -0700

    it works

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main block 
+ *
+ * Description: combinations of single numbers
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int g;
+
+	for (g = 0; g < 10; g++)
+	{
+	putchar('g');
+	putchar(',');
+	putchar(' ');
+	}
+	return (0);
+}
