0842231639dc99014d962aece4a00fbafd74fab8
semmygram <semiukayode254@gmail.com>
2022-09-13 10:01:41 -0700

    it works

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point 
+ *
+ * desc :  print the alphabets 10 times 
+ *
+ * return : void
+ *
+ */
+
+void print_alpahabet_x10(void)
+{
+	int I = 1, J;
+
+	for (I = 1; I <= 10; I++)
+	{
+		for (J = 'a'; J <= 'z'; J++)
+			_putchar(J);
+	}
+}
