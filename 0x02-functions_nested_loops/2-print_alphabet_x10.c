de5b5a57c8abe88a51d1c40a69485bd48901e321
semmygram <semiukayode254@gmail.com>
2022-09-13 12:09:44 -0700

    it works

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * Description: print the alphabets 10 times
- *
- * Return : void
- *
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *  *
+ *   * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
