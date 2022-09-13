760b71a433151649ebef482558101e2e1e780cd3
semmygram <semiukayode254@gmail.com>
2022-09-13 08:13:07 -0700

    it works

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,14 +11,14 @@
 
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	char printer[] = "_putchar";
+	int i = 0;
+
+	while (printer[i] != '\0')
+	{
+		_putchar(printer[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
