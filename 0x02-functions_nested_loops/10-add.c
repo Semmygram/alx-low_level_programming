10731561b10004e92932b6ba3b26fb40b39fdc4d
semmygram <semiukayode254@gmail.com>
2022-09-13 14:55:51 -0700

    it works

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * add - Adds two integers and returns
- * the result.
+ * add - Adds two integers and returnsthe number
  *
- * @a: number one.
- * @b: number two.
+ * @a: number one
+ * @b: number two
  *
- * Return: Add of number one and number two.
+ * Return: Add of number one and number two
  */
 int add(int a, int b)
 {
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * times_table - Prints the 9 times table
  *
@@ -5,25 +7,25 @@
  */
 void times_table(void)
 {
-	int a, b, op;
+	int a, b, j;
 
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
 		for (b = 1; b <= 9; b++)
 		{
-			op = a * b;
+			j = a * b;
 			_putchar(44);
 			_putchar(32);
-			if (op <= 9)
+			if (j <= 9)
 			{
 				_putchar(32);
-				_putchar(op + 48);
+				_putchar(j + 48);
 			}
 			else
 			{
-				_putchar((op / 10) + 48);
-				_putchar((op % 10) + 48);
+				_putchar((j / 10) + 48);
+				_putchar((j % 10) + 48);
 			}
 		}
 		_putchar('\n');
