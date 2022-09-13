65f0c8a6b9f0e70c633c0d0ab3d8cf55d4d7a0f4
Semmygram <111019992+Semmygram@users.noreply.github.com>
2022-09-13 21:12:17 +0000

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,21 @@
 #include "main.h"
-#include "holberton.h"
 
 /**
- *  * _abs - Computes the absolute value
- *   * of an integer.
- *    *
- *     * @i: input number as an integer.
- *      *
- *       * Return: absolute value
- *        */
+ * _abs - Computes the absolute value
+ * of an integer.
+ *
+ * @i: input number as an integer.
+ *
+ * Return: absolute value
+ */
 int _abs(int i)
 {
-		if (i >= 0)
-				{
-							return (i);
-								}
-			else
-					{
-								return (i * -1);
-									}
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
 }
