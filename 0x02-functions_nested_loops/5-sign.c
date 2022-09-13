a2084f64abb42ff9449d6b0355e1cf0babd3574a
semmygram <semiukayode254@gmail.com>
2022-09-13 13:39:33 -0700

    it works

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - print the sign of a number 
+ *
+ * @c :positive or negative nunbers 
+ *
+ * return : 1 or 0 or -1 depending on the output 
+ */
+int print_sign(int n)
+{
+	int n ;
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
