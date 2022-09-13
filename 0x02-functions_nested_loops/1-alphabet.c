b51bacbe43dee1f92b5494566fa7dc7738a60a96
semmygram <semiukayode254@gmail.com>
2022-09-13 09:06:11 -0700

    it works

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * description : prints the alphabet in lower case followed by a new line
+ *
+ * return : void 
+ */
+
+void print_alpahabet(void)
+{
+	int ch = a;
+
+	while (ch <= z)
+	{
+	_putchar(ch)
+	ch++;
+	}
+	return (0);
+}
