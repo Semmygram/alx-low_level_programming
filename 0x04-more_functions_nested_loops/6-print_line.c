f7682ba460875021642c66d713b103d688387040
semmygram <semiukayode254@gmail.com>
2022-09-15 13:53:01 -0700

    just ffor chilzzzzzzzzzzzzzzzz

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line 
+ *
+ * @n : length of the line 
+ *
+ * return : void 
+ *
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
