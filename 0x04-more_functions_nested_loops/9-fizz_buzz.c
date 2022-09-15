60d90cd3f72661f38c4c82793c5c6a999c83204b
semmygram <semiukayode254@gmail.com>
2022-09-15 15:16:57 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: prints fizz buzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+		printf(FizzBuzz);
+		}
+		else if (n % 3 == 0)
+		{
+		printf(Fizz);
+		}
+		else if (n % 5 == 0)
+		{
+		printf(Buzz);
+		}
+		else
+		{
+		printf(n);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
