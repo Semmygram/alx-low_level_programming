fde89ffc1805b9e0cdb4a1ebd718bff3d6d64b97
semmygram <semiukayode254@gmail.com>
2022-09-15 11:54:43 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for digit
+ *
+ * Description : checks if a number falls between 0 and 9
+ *
+ * @c:  digit to be checked
+ *
+ * Return: 1 if its a digit and 0 if not
+ */
+
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
