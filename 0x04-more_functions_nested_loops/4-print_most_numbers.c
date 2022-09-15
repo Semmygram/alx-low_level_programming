228ed63e73f2fe923ea4383761eb6cb66a7772fd
semmygram <semiukayode254@gmail.com>
2022-09-15 12:46:25 -0700

    normally

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers thru 0 - 9 minus 2 nd 4
+ *
+ * Return : 0 on sucess
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if !(i = 50 || i = 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
