f5dd77b4022bcc1c3e2de7ec82ce56f0f1c0dbb0
semmygram <semiukayode254@gmail.com>
2022-09-15 12:21:02 -0700

    jitgters

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	mul = a * b;
+	return (mul);
+}
